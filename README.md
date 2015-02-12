8d3b34306746ca88f2df8a065959d2e843f7cd58
Caleb <caleb.tutty@nzherald.co.nz>
2015-02-12 13:12:29 +1300

    Complete importer

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # mongodb-nz-census-geojson
 Import Census boundary GeoJSON into MongoDB
+
+To run:
+
+Edit the Bower dependencies in bower.json, i.e.:
+
+```json
+"dependencies": {
+  "nz_area_units_geojson":
+"git@github.com:nzherald/nz_area_units_geojson.git#~1.0.0"
+}
+```
+
+then `bower install`
+
+then `node index.js`
+
+Importing may take some time.
--- index.js
+++ index.js
@@ -6,4 +6,34 @@ var monk   = require('monk'),
 dotenv.load();
 var db = monk(process.env.DATABASE_URL || 'localhost/boundaries');
 
+var matchRegEx  = /bower_components\/nz_(.*)_geojson/,
+    matchString = 'bower_components/nz_*_geojson',
+    directories = glob.sync(matchString);
+
+var matches = directories.map(function(m) { return m.match(matchRegEx)[1]; });
+
+console.dir(matches);
+
+for(var m of matches) {
+  console.dir(m);
+  var collection = db.get(m),
+      files      = glob.sync('bower_components/nz_' + m + '_geojson/geojson/*.geojson')
+  for(var file of files) {
+    console.log('Importing ' + file);
+    var shape = JSON.parse(fs.readFileSync(file, 'utf-8')),
+        keys  = Object.keys(shape.features[0].properties),
+        vals  = [];
+
+    for(var key of keys) {
+      vals.push(shape.features[0].properties[key]);
+    }
+
+    var record = {
+      id: Number(vals[0]),
+      name: vals[1],
+      shape: shape
+    };
+    collection.insert(record);
+  };
+};
 
